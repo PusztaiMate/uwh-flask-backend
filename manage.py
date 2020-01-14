17f2cf597e1bd656533fce4321bb1a7a61387469
Máté Pusztai <mate.pusztai@ericsson.com>
2020-01-14 09:46:14 +0100

    Add seed_db command for testing

--- manage.py
+++ manage.py
@@ -1,6 +1,6 @@
 from flask.cli import FlaskGroup
 
-from project import app, db
+from project import app, db, Player, Training, Club
 
 
 cli = FlaskGroup(app)
@@ -10,6 +10,19 @@ cli = FlaskGroup(app)
 def recreate_db():
     db.drop_all()
     db.create_all()
+    db.session.commit()
+
+
+@cli.command("seed_db")
+def seed_db():
+    p = Player("Jakab", "Gipsz", "jakab@gipsz.hu")
+    db.session.add(p)
+    db.session.commit()
+    c = Club("Best VSE")
+    db.session.add(c)
+    db.session.commit()
+    t = Training(c.id, "2020-01-01T19:30:00", players=[p])
+    db.session.add(t)
     db.session.commit()
 
 
