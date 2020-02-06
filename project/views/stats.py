a9fe8882b8369e122fa8f58671d1c156f6d2b47b
Máté Pusztai <mate.pusztai@ericsson.com>
2020-02-06 13:22:52 +0100

    Add colored charts to player statistics

--- project/views/stats.py
+++ project/views/stats.py
@@ -18,12 +18,27 @@ def players():
     num_of_trainings = len(Training.query.all())
     players = []
     for p in raw_players:
+        train_perc = calculate_training_percentage(len(p.trainings), num_of_trainings)
+        color = determine_color(train_perc)
         players.append(
             {
                 "name": f"{p.lname} {p.fname}",
                 "num_trainings": len(p.trainings),
-                "training_percentage": int(len(p.trainings) / num_of_trainings * 100),
+                "training_percentage": train_perc,
+                "chart_color": color,
             }
         )
     players.sort(key=lambda p: p["name"])
     return render_template("players.html", players=players)
+
+
+def calculate_training_percentage(players_num: int, all_training_num: int) -> int:
+    return int(players_num / all_training_num * 100)
+
+
+def determine_color(percentage: int) -> str:
+    values = [(30, "is-danger"), (60, "is-warning"), (90, "is-success")]
+    for perc, color in values:
+        if percentage < perc:
+            return color
+    return "is-link"
